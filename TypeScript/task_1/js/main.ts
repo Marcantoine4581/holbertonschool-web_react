7ca3166865abaf295aedce53ecb9ed31d4b60b3f
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-08-01 08:24:06 +0000

    task 3

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -7,7 +7,7 @@ interface Teacher {
   [key: string]: any;
 }
 
-/* const teacher3: Teacher = {
+const teacher3: Teacher = {
   firstName: 'John',
   fullTimeEmployee: false,
   lastName: 'Doe',
@@ -15,17 +15,27 @@ interface Teacher {
   contract: false,
 };
 
-console.log(teacher3); */
+/* console.log(teacher3); */
 
 interface Directors extends Teacher {
   numberOfReports: number;
 }
 
-const director1: Directors = {
+/* const director1: Directors = {
   firstName: 'John',
   lastName: 'Doe',
   location: 'London',
   fullTimeEmployee: true,
   numberOfReports: 17,
 };
-console.log(director1);
+console.log(director1); */
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = function (firstName: string, lastName: string) {
+  return firstName.charAt(0) + ". " + lastName;
+}  
+
+console.log(printTeacher("John", "Doe"));
